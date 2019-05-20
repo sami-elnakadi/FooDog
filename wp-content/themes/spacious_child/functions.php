0be60c894a64bcb14d989bd49db61e4e2b11da78
user <sami.elnakadi@gmail.com>
2019-05-20 11:33:44 +0200

    categ

--- wp-content/themes/spacious_child/functions.php
+++ wp-content/themes/spacious_child/functions.php
@@ -5,3 +5,11 @@ add_action( 'wp_enqueue_scripts', 'wpchild_enqueue_styles' );
 function wpchild_enqueue_styles(){
   wp_enqueue_style( 'wpm-ruby-style', get_template_directory_uri() . '/style.css' );
 }
+
+function prefix_category_title( $title ) {
+  if ( is_category() ) {
+      $title = single_cat_title( '', false );
+  }
+  return $title;
+}
+add_filter( 'get_the_archive_title', 'prefix_category_title' );
